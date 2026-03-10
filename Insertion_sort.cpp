657c130e1fd7a9c2614a7bf90b8d282a12442027
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-10 11:42:14 +0700

    procedur insertionsort

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -21,8 +21,30 @@ void input (){
     cout << "==========Masukkan Element Array==========";
     cout << "==========================================";
 
-    for (int i = 0; i++ ){
+    for (int i = 0; i < n; i++ ){
         cout << "Data ke-" << (i+1) << ": ";
         cin >> arr[i];
+    }
+}
+
+void insertionsort(){
+    int temp;
+    int j;
+
+    for (int i = 0; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nStep" << i << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k]<< " ";
+        }
     }
 }
