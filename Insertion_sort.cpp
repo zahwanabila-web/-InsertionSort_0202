a4ad1148890b969d5da9d406f06f042adfdd80ac
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-10 11:51:44 +0700

    display

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -48,3 +48,15 @@ void insertionsort(){
         }
     }
 }
+
+void display(){
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElement Array" << endl;
+    cout << "\n=================";
+
+    for(int j = 0; j < n; j++){
+        
+    }
+    
+}
